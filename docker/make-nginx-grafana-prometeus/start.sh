26874a9e3100aee374d24c5e2d4a996697c49303
Henry Moon <hcmoon92@hotmail.com>
2024-10-25 01:43:29 +0900

    updated

--- docker/make-nginx-grafana-prometeus/start.sh
+++ docker/make-nginx-grafana-prometeus/start.sh
@@ -1,1 +1,6 @@
-docker run -d -p 8080:80 --name nginx-dashboard-container nginx-dashboard
+# Step 2: Set Up Nginx
+docker run -d -p 80:80 --name nginx-dashboard-container nginx-dashboard
+# Step 3: Set Up Prometheus
+docker run -d --name prometheus -p 9090:9090 -v ./prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+# Step 4: Set Up Grafana
+docker run -d --name=grafana -p 3000:3000 grafana/grafana
